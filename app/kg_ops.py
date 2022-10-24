8ac4e3deccdf8c18e9599fc7b614eb71ca8de79d
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2022-10-24 20:40:36 +0000

    fixes #14 and #19

--- app/kg_ops.py
+++ app/kg_ops.py
@@ -1,8 +1,30 @@
 from typing import List
 from rdflib import Graph, Namespace
 from rdflib import URIRef
-from rdflib.namespace import RDF, RDFS
+from rdflib.namespace import RDF, RDFS, DCTERMS
 from rdflib.plugins.sparql import prepareQuery
+
+SCHEMA = Namespace("https://schema.org/")
+DW = Namespace("http://dw.com/")
+
+NS_MAPPING = {
+    str(RDF.type): "type",
+    str(SCHEMA.url): "url",
+    str(RDFS.subClassOf): "subClass",
+    str(SCHEMA.applicationUrl): "applicationUrl",
+    str(SCHEMA.aboutUrl): "aboutUrl",
+    str(SCHEMA.publisher): "publisher",
+    str(SCHEMA.usageInfo): "usageInfo",
+    str(DCTERMS.subject): "subject",
+    str(DCTERMS.relation): "relation",
+    str(DCTERMS.hasPart): "hasPart",
+    str(RDFS.comment): "comment",
+    str(SCHEMA.name): "name",
+    str(SCHEMA.isPartOf): "isPartOf",
+    str(DW.parentNode): "parentNode",
+    str(DW.relatedMediaType): "relatedMediaType"
+
+}
 
 SCHEMA = Namespace("https://schema.org/")
 DW = Namespace("http://dw.com/")
@@ -11,252 +33,201 @@ g = Graph()
 g.parse("public/workflow.ttl")
 
 
-def assign_levels(all_nodes, all_links, begin_node_id, level, level_dict={}):
-    links = list(filter(lambda link: link['source'] == begin_node_id, all_links))
+class KIDGraph:
+    def __init__(self, click_history):
+        self.click_history = click_history
+        self.clicked_node = click_history[-1]
+        self.root_node = click_history[0]
+        self.node_ids = set()
+        self.links = list()
 
-    if len(links) == 0:
-        level_dict[begin_node_id] = level
-        # all_nodes.loc[all_nodes.id == begin_node_id, "level"] = level
+        self.node_ids.add(self.clicked_node)
 
-    for link in links:
-        level_dict[begin_node_id] = level
-        assign_levels(all_nodes, all_links, begin_node_id=link["target"], level=level + 1, level_dict=level_dict)
+    def assign_levels(self, begin_node_id, level, level_dict={}):
+        links = list(filter(lambda link: link['source'] == begin_node_id, self.links))
 
-    return level_dict
+        if len(links) == 0:
+            level_dict[begin_node_id] = level
 
+        for link in links:
+            level_dict[begin_node_id] = level
+            self.assign_levels(begin_node_id=link["target"], level=level + 1,
+                               level_dict=level_dict)
 
-def create_subgraph(click_history: List):
-    clicked_node_id = click_history[-1]
-    root_node_id = click_history[0]
+        return level_dict
 
-    root_node = node_features(root_node_id)
+    def construct(self):
+        self.get_children()
 
-    clicked_node = node_features(clicked_node_id)
+        if self.root_node != self.clicked_node:
+            self.get_parents()
 
-    parent_nodes, parent_links, children_nodes, children_links = [], [], [], []
-    children_nodes, children_links = get_children(clicked_node, root_node)
-    if root_node["id"] != clicked_node_id:
-        parent_nodes, parent_links = get_parents(clicked_node, root_node, click_history, children_nodes)
+        self.links = [dict(t) for t in {tuple(d.items()) for d in self.links}]
 
-    if len(parent_nodes) == 0:
-        parent_nodes.append(clicked_node)
+        level_dict = self.assign_levels(begin_node_id=self.root_node, level=0)
 
-    if len(children_nodes) == 0:
-        children_nodes = []
-        children_links = []
+        all_nodes = list(map(lambda node_id: self.get_feats(level_dict, node_id), self.node_ids))
 
-    all_nodes = parent_nodes + children_nodes
-    all_links = children_links + parent_links
-    all_links = [dict(t) for t in {tuple(d.items()) for d in all_links}]
+        return {
+            'nodes': all_nodes,
+            'links': self.links
+        }
 
-    del parent_nodes, children_nodes, parent_links, children_links
+    def get_feats(self, level_dict, node_id):
+        node_data = {}
+        if node_id == self.clicked_node:
+            for pred, obj in g.predicate_objects(URIRef(node_id)):
+                node_data[NS_MAPPING[str(pred)]] = str(obj)
+        node_data['id'] = node_id
+        node_data['name'] = str(g.value(URIRef(node_id), SCHEMA.name))
+        node_data['type'] = str(g.value(URIRef(node_id), RDF.type))
+        node_data['level'] = level_dict[node_id]
+        return node_data
 
-    level_dict = assign_levels(all_nodes, all_links, begin_node_id=root_node["id"], level=0)
+    def get_parents(self):
+        results = g.query(self.query_parents(),
+                          initBindings={'beginNode': URIRef(self.clicked_node), 'endNode': URIRef(self.root_node)})
 
-    for node in all_nodes:
-        node.update({"level": level_dict[node["id"]]})
+        if len(results) == 0:
+            sub_class_of_clicked_node = g.value(URIRef(self.clicked_node), DW["parentNode"])
+            begin_node = None
+            for subject in g.subjects(predicate=RDFS.subClassOf, object=sub_class_of_clicked_node):
+                if self.root_node == str(g.value(subject, DW["relatedMediaType"])):
+                    begin_node = subject
+                    pass
 
-    return {
-        'nodes': all_nodes,
-        'links': all_links
-    }
+            results_subclass = g.query(self.query_children(), initBindings={'beginNode': sub_class_of_clicked_node,
+                                                                            'endNode': URIRef(self.root_node)})
+            for result in results_subclass:
+                if str(result) not in self.node_ids:
+                    self.node_ids.add(str(result.childNode))
 
-
-def get_parents(clicked_node, root_node, click_history, children_nodes):
-    children_ids = [children_node["id"] for children_node in children_nodes]
-    results = g.query(query_parents(),
-                      initBindings={'beginNode': URIRef(clicked_node['id']), 'endNode': URIRef(root_node['id'])})
-    links = []
-    node_ids = set()
-
-    if len(results) == 0:
-        sub_class_of_clicked_node = g.value(URIRef(clicked_node["id"]), DW["parentNode"])
-        begin_node = None
-        for subject in g.subjects(predicate=RDFS.subClassOf, object=sub_class_of_clicked_node):
-            if root_node["id"] == str(g.value(subject, DW["relatedMediaType"])):
-                begin_node = subject
-                pass
-        results_subclass = g.query(query_children(), initBindings={'beginNode': sub_class_of_clicked_node,
-                                                                   'endNode': URIRef(root_node['id'])})
-        for result in results_subclass:
-            if str(result) not in children_ids:
-
-                    node_ids.add(str(result.childNode))
-
-                    links.append(
+                    self.links.append(
                         {
                             'source': str(begin_node),
                             'target': str(result.childNode)
                         }
                     )
-        results = g.query(query_parents(), initBindings={'beginNode': begin_node, 'endNode': URIRef(root_node['id'])})
+            results = g.query(self.query_parents(),
+                              initBindings={'beginNode': begin_node, 'endNode': URIRef(self.root_node)})
 
-    for idx, result in enumerate(results):
+        for result in results:
 
-        if str(result.parentOfParentNode) not in click_history or str(result.parentOfBeginNode) not in click_history:
-            continue
-        #
-        if str(result.parentOfParentRelatedMediaType) != root_node['id'] and result.parentOfParentRelatedMediaType:
-            continue
+            if str(result.parentOfParentNode) not in self.click_history or str(
+                    result.parentOfBeginNode) not in self.click_history:
+                continue
+            #
+            if str(result.parentOfParentRelatedMediaType) != self.root_node and result.parentOfParentRelatedMediaType:
+                continue
 
-        if str(result.parentOfBeginNodeRelatedMediaType) != root_node[
-            'id'] and result.parentOfBeginNodeRelatedMediaType:
-            continue
+            if str(result.parentOfBeginNodeRelatedMediaType) != self.root_node and result.parentOfBeginNodeRelatedMediaType:
+                continue
 
-        node_ids.add(str(result.parentOfParentNode))
-        node_ids.add(str(result.parentOfBeginNode))
+            self.node_ids.add(str(result.parentOfParentNode))
+            self.node_ids.add(str(result.parentOfBeginNode))
 
-        links.append(
-            {
-                'source': str(result.parentOfParentNode),
-                'target': str(result.parentOfBeginNode)
-            }
-        )
-
-        if str(result.childNodeMediaType) != root_node[
-            'id'] and result.childNodeMediaType:
-            continue
-
-        # add child nodes of subParent nodes
-
-        if str(result.childNode) not in node_ids and str(result.childNode) not in children_ids:
-            node_ids.add(str(result.childNode))
-
-            links.append(
+            self.links.append(
                 {
                     'source': str(result.parentOfParentNode),
-                    'target': str(result.childNode)
+                    'target': str(result.parentOfBeginNode)
                 }
             )
 
-    if not node_ids:
-        return [], []
+            if str(result.childNodeMediaType) != self.root_node and result.childNodeMediaType:
+                continue
 
-    nodes = [node_features(node_id) for node_id in node_ids]
-    return nodes, links
+            # add child nodes of subParent nodes
+            if str(result.childNode) not in self.node_ids:
+                self.node_ids.add(str(result.childNode))
 
+                self.links.append(
+                    {
+                        'source': str(result.parentOfParentNode),
+                        'target': str(result.childNode)
+                    }
+                )
 
-def query_parents():
-    query = '''
-    SELECT ?parentOfBeginNode ?parentOfParentNode ?childNode ?parentOfParentRelatedMediaType ?parentOfBeginNodeRelatedMediaType ?childNodeMediaType
-    WHERE
-    {
-        ?beginNode dw:parentNode* ?parentOfBeginNode .
-        ?parentOfBeginNode ?y ?parentOfParentNode .
-        OPTIONAL {
-            ?childNode dw:parentNode ?parentOfParentNode .
-        }
-        OPTIONAL {
-               ?parentOfParentNode a dw:Task.
-               ?parentOfParentNode dw:relatedMediaType ?parentOfParentRelatedMediaType .
-        }
-        OPTIONAL {
-               ?parentOfBeginNode a dw:Task.
-               ?parentOfBeginNode dw:relatedMediaType ?parentOfBeginNodeRelatedMediaType .
-        }
-        
-        OPTIONAL {
-               ?childNode a dw:Task.
-               ?childNode dw:relatedMediaType ?childNodeMediaType .
-        }
-        
-        FILTER(?y = dw:parentNode)
-        ?parentOfParentNode dw:parentNode* ?endNode .
-    }
-    '''
-    q = prepareQuery(query,
-                     initNs={"dw": DW},
-                     )
-    return q
-
-
-def get_children(parent_node, root_node):
-    q = query_children()
-
-    results = g.query(q, initBindings={'beginNode': URIRef(parent_node['id']), 'endNode': URIRef(root_node['id'])})
-
-    if len(results) == 0:
+    def query_parents(self):
         query = '''
-         SELECT ?childNode ?relatedMediaType ?parentClassNode ?beginNode
-         WHERE
-         {  
-            ?beginNode rdfs:subClassOf ?parentClassNode .
-            ?childNode dw:parentNode ?parentClassNode .
+        SELECT ?parentOfBeginNode ?parentOfParentNode ?childNode ?parentOfParentRelatedMediaType ?parentOfBeginNodeRelatedMediaType ?childNodeMediaType
+        WHERE
+        {
+            ?beginNode dw:parentNode* ?parentOfBeginNode .
+            ?parentOfBeginNode ?y ?parentOfParentNode .
+            OPTIONAL {
+                ?childNode dw:parentNode ?parentOfParentNode .
+            }
+            OPTIONAL {
+                   ?parentOfParentNode a dw:Task.
+                   ?parentOfParentNode dw:relatedMediaType ?parentOfParentRelatedMediaType .
+            }
+            OPTIONAL {
+                   ?parentOfBeginNode a dw:Task.
+                   ?parentOfBeginNode dw:relatedMediaType ?parentOfBeginNodeRelatedMediaType .
+            }
             
+            OPTIONAL {
+                   ?childNode a dw:Task.
+                   ?childNode dw:relatedMediaType ?childNodeMediaType .
+            }
+            
+            FILTER(?y = dw:parentNode)
+            ?parentOfParentNode dw:parentNode* ?endNode .
+        }
+        '''
+        q = prepareQuery(query,
+                         initNs={"dw": DW},
+                         )
+        return q
+
+    def query_children(self):
+        query = '''
+         SELECT ?childNode ?relatedMediaType ?beginNode
+         WHERE
+         {
+            ?childNode dw:parentNode ?beginNode
             OPTIONAL{
-             ?parentClassNode dw:parentNode* ?parentOfBeginNode .
+             ?beginNode dw:parentNode* ?parentOfBeginNode .
              ?parentOfBeginNode ?y ?parentOfParentNode .
              ?parentOfParentNode dw:parentNode* ?endNode .
              }
-
+    
             OPTIONAL {
-               ?parentClassNode a dw:Task .
-               ?parentClassNode dw:relatedMediaType ?relatedMediaType .
+               ?childNode a dw:Task .
+               ?childNode dw:relatedMediaType ?relatedMediaType .
             }
-
+    
          }
          '''
         q = prepareQuery(query,
                          initNs={"dw": DW, "rdfs": RDFS},
                          )
+        return q
 
-        results = g.query(q, initBindings={'beginNode': URIRef(parent_node['id']), 'endNode': URIRef(root_node['id'])})
+    def get_children(self):
+        q = self.query_children()
+        results = g.query(q, initBindings={'beginNode': URIRef(self.clicked_node), 'endNode': URIRef(self.root_node)})
 
-    links = None
-    node_ids = None
-    for idx, result in enumerate(results):
-        if idx == 0:
-            links = []
-            node_ids = set()
+        if len(results) == 0:
+            results = g.query(q, initBindings={'beginNode': g.value(URIRef(self.clicked_node), RDFS.subClassOf),
+                                               'endNode': URIRef(self.root_node)})
 
-        if str(result.relatedMediaType) != root_node['id'] and result.relatedMediaType:
-            continue
+        for result in results:
+            if str(result.relatedMediaType) != self.root_node and result.relatedMediaType:
+                continue
 
-        node_ids.add(str(result.childNode))
+            self.node_ids.add(str(result.childNode))
 
-        links.append(
-            {
-                'source': parent_node['id'],
-                'target': str(result.childNode)
-            }
-        )
+            self.links.append(
+                {
+                    'source': self.clicked_node,
+                    'target': str(result.childNode)
+                }
+            )
 
-    if not node_ids:
-        return [], []
-
-    nodes = [node_features(node_id) for node_id in node_ids]
-
-    return nodes, links
-
-
-def query_children():
-    query = '''
-     SELECT ?childNode ?relatedMediaType ?beginNode
-     WHERE
-     {
-        ?childNode dw:parentNode ?beginNode
-        OPTIONAL{
-         ?beginNode dw:parentNode* ?parentOfBeginNode .
-         ?parentOfBeginNode ?y ?parentOfParentNode .
-         ?parentOfParentNode dw:parentNode* ?endNode .
-         }
-    
-        OPTIONAL {
-           ?childNode a dw:Task .
-           ?childNode dw:relatedMediaType ?relatedMediaType .
+    def node_features(self, node_id):
+        return {
+            'id': str(node_id),
+            'name': str(g.value(URIRef(node_id), SCHEMA.name)),
+            'type': str(g.value(URIRef(node_id), RDF.type))
         }
-        
-     }
-     '''
-    q = prepareQuery(query,
-                     initNs={"dw": DW, "rdfs": RDFS},
-                     )
-    return q
-
-
-def node_features(node_id):
-    return {
-        'id': str(node_id),
-        'name': str(g.value(URIRef(node_id), SCHEMA.name)),
-        'type': str(g.value(URIRef(node_id), RDF.type))
-    }
