26b7909db00027ca084e4f20464c5985a0f5412a
tlmn <mail@tlmn.io>
2022-11-28 12:28:47 +0100

    add CORS

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 load_dotenv()
 CACHE = Cache('tmp')
 
-origins = ["*"]
+origins = ["https://preview.howtoverify.info", "http://localhost:3000", "http://localhost:3001"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
